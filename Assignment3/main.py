900538116c756beed85b123e041c2fbba27d4dc3
mahmoudchouman <mahmoud-chouman@hotmail.com>
2023-10-26 11:45:19 +0300

    refactored elements input and made it global

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -6,28 +6,31 @@ matrix1 = []
 rows2 = int(input("Enter the number of rows: "))
 columns2 = int(input("Enter the number of columns: "))
 matrix2 = []
-def addMatrices():
-    #first we ensure that the matrices are equal with this if statement
-    if rows1 != rows2 or columns1 != columns2:
-         print("Please run the program again and insert equal matrices")
-
-    # We ask the user to enter the elements of the matrix and we iterate through the rows/columns then appending the
+# We ask the user to enter the elements of the matrix and we iterate through the rows/columns then appending the
     # elements to the matrix
-    for row in range(rows1):
-        row_list = []
-        for col in range(columns1):
+for row in range(rows1):
+    row_list = []
+    for col in range(columns1):
             element = input("Enter element {} of row {}: ".format(col + 1, row + 1))
             row_list.append(element)
             matrix1.append(row_list)
     print(matrix1)
 
-    for row in range(rows2):
-        row_list = []
-        for col in range(columns2):
-            element = input("Enter element {} of row {}: ".format(col + 1, row + 1))
-            row_list.append(element)
-            matrix2.append(row_list)
+for row in range(rows2):
+    row_list = []
+    for col in range(columns2):
+        element = input("Enter element {} of row {}: ".format(col + 1, row + 1))
+        row_list.append(element)
+        matrix2.append(row_list)
+
     print(matrix2)
+
+def addMatrices():
+    #first we ensure that the matrices are equal with this if statement
+    if rows1 != rows2 or columns1 != columns2:
+        print("Please run the program again and insert equal matrices")
+        exit()
+
 
 
 addMatrices()
