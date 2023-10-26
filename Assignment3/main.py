4dde3e1c81cbc825686c1489f3d1890b1b2224b2
mahmoudchouman <mahmoud-chouman@hotmail.com>
2023-10-26 09:23:30 +0300

    initial thoughts

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+
+def addMatrices(matrix1,matrix2):
+    rows1 = int(input("Enter the number of rows: "))
+    columns1 = int(input("Enter the number of columns: "))
+    matrix1 = []
+    for row in range(rows1):
+        raw_list = []
+        for col in range(columns1):
+           matrix1.append(raw_list)
+
+
+addMatrices([1,2,3],[4,5,6])
+
+
