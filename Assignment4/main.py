e31ac08fa73ad43b1713b820ecf4c0241150ff48
mahmoudchouman <mahmoud-chouman@hotmail.com>
2023-11-19 15:47:31 +0200

    preparing the menu

---
+++ Assignment4/main.py
@@ -0,0 +1,25 @@
+def displayMenu():
+    print("Menu:")
+    print("1-Singly Linked List")
+    print("2-Check if Palindrome")
+    print("3-Priority Queue")
+    print("4-Evaluate an infix Expression")
+    print("5-Graph")
+    print("6-Exit")
+
+
+
+def main():
+    name = input("What is your name?: ")
+    print("Thank you for using my program" , name)
+
+    while True:
+        displayMenu()
+        choice = input("Please enter a choice between 1 and 6: ")
+
+
+
+
+
+
+main()
